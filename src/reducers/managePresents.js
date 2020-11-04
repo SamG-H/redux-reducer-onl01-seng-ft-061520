7fd31ddafeb5476214b0a691dd351d8ae2c3f74d
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-04 10:30:13 -0800

    commit tree 64777617e7cfa029f9825cc545db6bb38d35e710

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
 export function managePresents(state, action){
+    switch(action.type) {
+      case "INCREASE":
+        return { numberOfPresents: state + 1}
+      default:
+        return { numberOfPresents: 0}
+    }
 }
